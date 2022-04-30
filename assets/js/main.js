1134c0edbd4bcee422d160c36b3e51fcee6435e0
Laurenz19 <laurenziosambany@gmail.com>
2022-04-30 17:09:59 +0300

    adding coding skill chart

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,9 +90,73 @@ function slideResume(item, type){
     item.removeClass('item-left')
     item.removeClass('item-right')
    
-    if(type == 'prev'){
+    if(type == 'next'){
        item.addClass('item-left')
     }else{
        item.addClass('item-right')
     }
+}
+
+/** Skills **/
+let skills_template = $("#all-skills-template")
+let skills_template_html = skills_template.html()
+let skills = ''
+let data = [
+    {
+        name: "HTML & CSS", 
+        value: 80,
+        text: "As I want to be a web integrator, this skill is really important.",
+    },
+    {
+        name: "JavaScript", 
+        value: 70,
+        text: "I know a little bit about its frameworks such as Jquery and Vue.js.",
+    },
+    {
+        name: "Server Side", 
+        value: 75,
+        text: " As back-end technology, I use node.js and also a php framework like Symfony.",
+    }
+]
+
+renderSkills(data)
+function renderSkills(data){
+    skills = ""
+    let all_skills =''
+    let custom_class = []
+    for (let i = 0; i < data.length; i++) {
+      
+        all_skills += '<div class="skill-item">'+
+        '<div class="outer">' +
+            '<div class="inner">' +
+                `<div class="number">${data[i].value}%</div>`+
+            '</div>'+
+        '</div>'+
+        '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">'+
+            '<defs>'+
+                '<linearGradient id="GradientColor">'+
+                '<stop offset="0%" stop-color="rgb(4, 34, 113)" /><stop offset="100%" stop-color="rgb(5, 42, 75)" />'+
+                '</linearGradient>'+
+            '</defs>'+
+            `<circle class="skill${i}" cx="80" cy="80" r="38" stroke-linecap="round" />`+
+        '</svg>'+
+        `<div class="skill-name">${data[i].name}</div>`+
+        '<div class="skill-text">'+
+            `${data[i].text}`+ 
+        '</div>'+
+    '</div>' 
+       custom_class.push({value: data[i].value, name: `.skill${i}`})
+    }
+
+
+    skills += skills_template_html.replace(/{{skills}}/g, all_skills)
+    $("#skills").html(skills)
+
+    for (let i = 0; i < custom_class.length; i++) {
+        console.log(300*custom_class[i].value/100)
+        $(custom_class[i].name).css({
+            "stroke-dasharray": 235*custom_class[i].value/100
+        })
+    }
+    
 }
